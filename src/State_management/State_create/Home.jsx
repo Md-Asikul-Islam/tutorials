22f1ae75a44e765222350ce47d99721b08b81155
Md-Asikul-Islam <mdasikuli48@gmail.com>
2023-03-06 23:15:26 +0600

    this is my first projent

--- src/State_management/State_create/Home.jsx
+++ src/State_management/State_create/Home.jsx
@@ -32,3 +32,31 @@ export default class Home extends Component {
     )
   }
 }
+
+
+//***/   ১। state হল Component এর নিজস্ব ডাটা  এবং  ডাটা গুলা পরিবর্তন করা যায় ।
+
+// ২। constructor অবজেক্ট এর কোন ডাটা রেনডার ফাংশনে এক্সেস করতে চাইলে (this.state.objectName) দিয়ে  এক্সেস নিতে হবে ।
+
+// ৩। state এর কোন ডাটা পরিবর্তন করতে setState() ফাংশনের মধ্যে করতে হবে ।
+
+// ৪।  class এর ভিতরে কোন মেথড কে এক্সেস করতে অন্য মেথডে এক্সেস করতে চাইলে this.methodName  দিয়ে এক্সেস করতে হবে ।   
+
+// ৫।  // Wrong
+// this.setState({
+//   counter: this.state.counter + this.props.increment,
+// }); এখানে counter: এর পর কখনই this.state ব্যবহার করা যাবে না ।
+
+
+// তখন এভাবে করতে হবে 
+// // Correct
+// this.setState(function(state, props) {
+//     return {
+//       counter: state.counter + props.increment
+//     };
+//   });
+
+// ৬। setState কোন অবজেক্ট দিলে পূর্বের অবজেক্টএর সাথে তুলনা করে যতটুকই পরিবর্তন হইসে ঠিক ঐ টাই আপডেট করে দিবে বা মারজ করবে
+
+
+// ******
